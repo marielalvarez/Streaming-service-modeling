e672c4979e79e841cd9743011aada96aff4fde36
SamiraHr5 <samirahazimr@gmail.com>
2024-06-13 20:34:31 -0600

    Update Movies.cpp

--- Movies.cpp
+++ Movies.cpp
@@ -12,3 +12,16 @@
 
 Movies::Movies() : Video() {}
 Movies::Movies(int ID, string title, int duration, string genre, double rating) : Video(ID, title, duration, genre, rating) {}
+
+void Movies::showData() {
+  cout << endl;
+  cout << "Movies" << endl;
+  cout << "ID: " << ID << endl;
+  cout << "Title: " << title << endl;
+  cout << "Duration: " << duration << endl;
+  cout << "Genre: " << genre << endl;
+  cout << "Rating: " << rating << endl;
+  cout << endl;
+}
+
+#endif
