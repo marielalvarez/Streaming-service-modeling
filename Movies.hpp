b9a167a8ec4c8df4aae14032647c1491f4bbe268
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 16:24:14 -0600

    Update Movies.hpp

--- Movies.hpp
+++ Movies.hpp
@@ -4,21 +4,15 @@
 //
 //  Created by Mariel Alvarez Salas on 13/06/24.
 //
+#ifndef MOVIES_HPP
+#define MOVIES_HPP
 
-#ifndef Movies_hpp
-#define Movies_hpp
-#include <string>
-#include <stdio.h>
 #include "Video.hpp"
 
-using namespace std;
-
-class Movies : public Video{
+class Movies : public Video {
 public:
-    Movies();
-    Movies(int ID, string title, int duration, string genre, double rating);
-    //Redefined method
-    void showData();
+    Movies(int id, std::string title, int duration, std::string genre, double rating);
+    void showData() const override;
 };
 
-#endif /* Movies_hpp */
+#endif
