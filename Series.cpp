78e9efa1e5a936722244e23c2b7fbc86afd8be82
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 16:23:54 -0600

    Update Series.cpp

--- Series.cpp
+++ Series.cpp
@@ -5,58 +5,35 @@
 //  Mariel Alvarez Salas
 //  Samira Hazim Rodriguez
 //
-#include <iostream>
 #include "Series.hpp"
-#include "Episodes.hpp"
+#include <iostream>
 
-using namespace std;
-//Constructors
-Series::Series(){
-    ID = 0;
-    title = "-";
-    episode = {};
-    seasons = 0;
-}
-Series::Series(int ID, string title, int seasons){
-    this->ID = ID;
-    this->title=title;
-    this->seasons=seasons;
-}
-Series::Series(int ID, string title, vector<Episodes*> episode, int seasons){
-    this->ID = ID;
-    this->title=title;
-    this->episode=episode;
-    this->seasons=seasons;
-}
+Series::Series(int id, std::string title, int seasons)
+    : Video(id, title, 0, "", 0.0), seasons(seasons) {}
 
-//Setters & Getters
-void Series::setID(int ID){this->ID = ID;}
-void Series::setTitle(string title){this->title = title;}
-void Series::setEpisode(vector<Episodes*> episode){this->episode = episode;}
-void Series::setSeason(int seasons){this-> seasons = seasons;}
-
-int Series::getID(){return ID;}
-string Series::getTitle(){return title;}
-vector<Episodes*> Series::getEpisode(){return episode;}
-int Series::getSeason(){return seasons;}
-
-//Methods
-void Series::showSeriesData(){
-    cout << endl<< "ID:" << ID <<endl;
-    cout<< "Title:" << title <<endl;
-    cout<< "Number of seasons:" << seasons <<endl;
-}
-void Series::showEpisodes() {
-    std::vector<Episodes*>::const_iterator i;
-    for(i = episode.begin(); i != episode.end(); ++i) {
-        (*i)->showData();
+Series::~Series() {
+    for (Episodes* episode : episodes) {
+        delete episode;
     }
 }
-void Series::showEpisodesWRating(double rating) {
-    std::vector<Episodes*>::const_iterator i;
-    for (i = episode.begin(); i != episode.end(); ++i) {
-        if ((*i)->getRating() == rating) {
-            (*i)->showData();
+
+int Series::getSeasons() const {
+    return seasons;
+}
+
+void Series::addEpisode(Episodes* episode) {
+    episodes.push_back(episode);
+}
+
+void Series::showData() const {
+    std::cout << "Series [ID: " << id << ", Title: " << title << ", Seasons: " << seasons << "]" << std::endl;
+}
+
+void Series::showEpisodesWRating(double rating) const {
+    std::cout << "Episodes with rating " << rating << ":" << std::endl;
+    for (Episodes* episode : episodes) {
+        if (episode->getRating() == rating) {
+            episode->showData();
         }
     }
 }
