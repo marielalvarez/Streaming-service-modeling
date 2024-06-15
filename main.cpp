9f6219460dc54a5d1922c22774221fa6b07989f5
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 13:20:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,105 +14,112 @@
 #include "Movies.hpp"
 #include "Video.hpp"
 
-
 using namespace std;
+
 void loadData(vector<Video*>& videos, vector<Series*>& nameSeries) {
-    
     string seriesFile = "series.txt";
     string moviesFile = "movies.txt";
-    string episodosFile = "episodes.txt";
+    string episodesFile = "episodes.txt";
+
     ifstream archivoSeries(seriesFile);
     ifstream archivoMovies(moviesFile);
     ifstream archivoEpisodes(episodesFile);
-    
+
     if (archivoSeries.is_open()) {
         string readline;
-        
         int id;
         string titleS;
         int temps;
-        
+
         while (getline(archivoSeries, readline)) {
             stringstream ss(readline);
             ss >> id >> titleS >> temps;
-            nameSeries.push_back(new Series(id, titleS, temps)); // se crea un nuevo objeto de tipo Serie y
-            // se agrega a un vector de apuntadores
+            nameSeries.push_back(new Series(id, titleS, temps));
         }
         archivoSeries.close();
         cout << "Series data loaded correctly" << endl;
     }
+
     if (archivoMovies.is_open()) {
         string readlin;
-        
         int Id;
         string titleM;
         int duracion;
         string genero;
         double calif;
-        
-        while (getline(archivoSeries, readlin)) {
+
+        while (getline(archivoMovies, readlin)) {
             stringstream ss(readlin);
             ss >> Id >> titleM >> duracion >> genero >> calif;
             videos.push_back(new Movies(Id, titleM, duracion, genero, calif));
         }
-        archivoSeries.close();
+        archivoMovies.close();
         cout << "Movies data loaded correctly" << endl;
     }
+
     if (archivoEpisodes.is_open()) {
         string readli;
-        
         int iD;
         string idserie;
         string nameE;
         int dur;
         double cali;
         string temp;
-        
-        while (getline(archivoSeries, readli)) {
+
+        while (getline(archivoEpisodes, readli)) {
             stringstream ss(readli);
             ss >> iD >> idserie >> nameE >> dur >> cali >> temp;
-            videos.push_back(new Episodes(iD, idserie, nameE, dur, cali, temp));
+            Episodes* episode = new Episodes(iD, idserie, nameE, dur, cali, temp);
+
+            // Find the corresponding series and add the episode
+            for (Series* series : nameSeries) {
+                if (series->getId() == stoi(idserie)) {
+                    series->addEpisode(episode);
+                    break;
+                }
+            }
+
+            videos.push_back(episode);
         }
-        archivoSeries.close();
+        archivoEpisodes.close();
         cout << "Episodes data loaded correctly" << endl;
     }
 }
 
-void showVideosbyratingandgenre(vector<Video*>& videos, int rating, string genre) {
+void showVideosbyratingandgenre(vector<Video*>& videos, double rating, string genre) {
     cout << "Videos with ratings " << rating << " and genres " << genre << ":" << endl;
     for (Video* video : videos) {
-        if (video->getrating() == rating &&
-            video->getgenre() == genre) {video->showData();
+        if (video->getRating() == rating && video->getGenre() == genre) {
+            video->showData();
         }
     }
+    cout << "We couldn't find a video that has the specifications inserted" << endl;
 }
-
 
 void showEpRating(vector<Series*>& nameSeries, const string& title, double rating) {
-    cout << "Episodios de la Serie " << title << " con Calificación " << rating << ":" << endl;
-    for (Series* name : nameSeries) {
-        if (name != nullptr && name->getTitle() == title) {
-            name->showEpisodesWRating(rating);
+    cout << "Episodes of the series " << title << " with rating " << rating << ":" << endl;
+    for (Series* series : nameSeries) {
+        if (series != nullptr && series->getTitle() == title) {
+            series->showEpisodesWRating(rating);
         }
     }
 }
 
-void showMoviesByRating(vector<Video*>& videos,
-    double rating) {
+void showMoviesByRating(vector<Video*>& videos, double rating) {
     cout << "Movies with ratings " << rating << ":" << endl;
-    for (Movies* movies : movies) {
-        Movies* movies = dynamic_cast<Movies*>(movies);
-        if (movie != nullptr && movie->getrating() == rating) {
-            movies->showData();
+    for (Video* video : videos) {
+        Movies* movie = dynamic_cast<Movies*>(video);
+        if (movie != nullptr && movie->getRating() == rating) {
+            movie->showData();
         }
     }
 }
 
 void ratevideo(vector<Video*>& videos, string title, int value) {
     for (Video* video : videos) {
-        if (video->getTitle() == titulo) {
+        if (video->getTitle() == title) {
             video->setRating(value);
-            cout << "The video: \"" << titulo << "\" has been rate with value " << value << endl;
+            cout << "The video: \"" << title << "\" has been rated with value " << value << endl;
             return;
         }
     }
@@ -123,26 +130,23 @@ int main() {
     vector<Video*> videos;
     vector<Series*> nameSeries;
 
-
-    // Ciclo de opciones del menú
     int option;
     do {
-        cout << "1.  Load data file." << endl;
+        cout << "1. Load data file." << endl;
         cout << "2. Show the videos with a specific rating or from a specific genre." << endl;
         cout << "3. Show the episodes of a specific series with a specific rating." << endl;
         cout << "4. Show the movies with a specific rating." << endl;
         cout << "5. Rate a video." << endl;
-        cout << "6. Ask for the title to rate." << endl;
-        cout << "7. Ask for the rating." << endl;
-        cout << "8. Exit" << endl;
+        cout << "6. Exit" << endl;
         cin >> option;
 
         switch (option) {
             case 1: {
                 loadData(videos, nameSeries);
+                break;
             }
             case 2: {
-                int rating;
+                double rating;
                 string genre;
                 cout << "Insert rating: ";
                 cin >> rating;
@@ -154,7 +158,7 @@ int main() {
             case 3: {
                 string seriesTitle;
                 double rating;
-                cout << "Enter series title: ";
+                cout << "Enter series title (Use _ for spaces): ";
                 cin >> seriesTitle;
                 cout << "Enter rating: ";
                 cin >> rating;
@@ -165,7 +169,7 @@ int main() {
                 double rating;
                 cout << "Enter rating: ";
                 cin >> rating;
-                showMoviesByRating(movies, rating);
+                showMoviesByRating(videos, rating);
                 break;
             }
             case 5: {
@@ -174,8 +178,8 @@ int main() {
                 cout << "Insert what title you want to rate: ";
                 cin >> title;
                 cout << "Insert the rating: ";
-                cin >> value;
-                ratevideo(videos, title, value);
+                cin >> rate;
+                ratevideo(videos, title, rate);
                 break;
             }
             case 6: {
